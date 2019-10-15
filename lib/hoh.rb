61275b78f1916107257b8ba0101f9dabd4007f98
Alexander Williams <williams_alex@protonmail.com>
2019-10-15 16:13:08 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,34 +1,34 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+ Build a nested HoH of the classification of biological life on Earth based
+ on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
+ that HoH as the return value for the method "naming_system."
+
+ The tests will guide your construction.
+
+ PORTION_1 = {
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class"
+     }
+   }
+ }
+ 
+ PORTION_2 = {
+   label: "Order"
+ }
+
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ }
+ 
+ PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+ }
 
 def naming_system
   # Remember:
@@ -41,5 +41,7 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-
+  naming = {Kingdom => {Phylum => {Class => {Order => {Family => {Genus => {Species => 'ad hominem'}}}}}}}
+  classes = {PORTION_1 => PORTION_2 => PORTION_3 => PORTION_4}
+  classes
 end
